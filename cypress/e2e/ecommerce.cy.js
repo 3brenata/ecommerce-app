2fefd4d9feae571c5c6a43179a5279bc20a5d945
Fabricio Aquiles <70663657+fabricioaquiles@users.noreply.github.com>
2025-05-14 07:54:01 -0300

    Add files via upload

---
+++ cypress/e2e/ecommerce.cy.js
@@ -0,0 +1,24 @@
+describe('Fluxo de Interação no E-commerce', () => {
+
+  it('Deve permitir que o usuário navegue até a página de produtos', () => {
+    cy.visit('http://localhost:3000');
+    cy.get('#products-list li').should('have.length.greaterThan', 0); // Verifica se há produtos listados
+  });
+
+  it('Deve exibir os botões de "Adicionar ao Carrinho"', () => {
+    cy.visit('http://localhost:3000');
+    cy.get('#products-list li button').should('contain', 'Adicionar ao Carrinho'); // Verifica se o botão está presente
+  });
+
+  it('Deve permitir que o usuário clique no botão de "Adicionar ao Carrinho"', () => {
+    cy.visit('http://localhost:3000');
+    cy.get('#products-list li').first().find('button').click(); // Simula o clique no primeiro botão
+  });
+
+  it('Deve clicar no segundo botao de "Adicionar ao carrinho"', () => {
+    cy.visit('http://localhost:3000');
+    cy.get('#products-list li').eq(1).find('button').click();
+    
+  })
+
+})
